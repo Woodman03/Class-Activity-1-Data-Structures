77ef4cee56bac5359c7d3edb2930310c18277081
Dillon Wood <dwood16@atu.edu>
2025-01-23 10:36:23 -0600

    Commit 2

--- main.cpp
+++ main.cpp
@@ -25,17 +25,20 @@ int main()
     //     string lastName;
 
 
-    //     cout << "Please enter the student's first name: ";
+    //     cout << "Please enter the first student's first name: ";
     //     cin >> firstName;
-    //     cout << "Please enter the student's last name: ";
+    //     cout << "Please enter the first student's middle name: ";
+    //     cin >> middleName;
+    //     cout << "Please enter the first student's last name: ";
     //     cin >> lastName;
 
     //   personType student2;
 
-    //   student2.setName(firstName,lastName);
+    //   student2.setName(firstName, middleName, lastName);
 
 
     //     cout << "Second student's first name: " << student2.getFirstName() << endl;
+    //     cout << "Second student's middle name: " << student2.getMiddleName() << endl;
     //     cout << "Second Student's last name: " << student2.getLastName() << endl;
     return 0;
 }
